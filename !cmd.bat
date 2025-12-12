d1f91fad1504b13588269b8c369ec332b556981c
s0911602565 <s0911602565@gmail.com>
2025-12-12 18:18:05 +0800

    1

--- !cmd.bat
+++ !cmd.bat
@@ -1,5 +1,5 @@
-git pull https://github.com/s0911602565/_jquery.git
+git pull https://github.com/s0911602565/_SQL-Server.git
 git add .
 git commit -m "1"
-git push -u _jquery master 
+git push -u origin master 
 pause
